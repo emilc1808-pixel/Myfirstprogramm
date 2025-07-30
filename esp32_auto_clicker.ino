61e4d33e5a91248224e96416b57314f31ad0b1ca
emilc1808-pixel <emilc1808@gmail.com>
2025-07-30 15:53:44 +0200

    1000hz to 10000hz

--- esp32_auto_clicker.ino
+++ esp32_auto_clicker.ino
@@ -12,9 +12,10 @@ bool lastButtonState = HIGH;
 bool currentButtonState = HIGH;
 bool isClicking = false;
 
-// Timing-Variablen für 1000 Hz (1ms Intervall)
+// Timing-Variablen für 10 kHz (0.1ms Intervall)
 unsigned long lastClickTime = 0;
-const unsigned long clickInterval = 1; // 1ms = 1000 Hz
+const unsigned long clickIntervalMicros = 100; // 100 Mikrosekunden = 10 kHz
+unsigned long clickCounter = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -27,8 +28,9 @@ void setup() {
   Mouse.begin();
   USB.begin();
   
-  Serial.println("ESP32-S3 Auto Clicker gestartet!");
-  Serial.println("Drücke den Button um Auto-Click zu starten/stoppen");
+  Serial.println("ESP32-S3 High Speed Auto Clicker gestartet!");
+  Serial.println("Drücke den Button um Auto-Click zu starten/stoppen (10 kHz)");
+  Serial.println("10.000 Klicks pro Sekunde - sehr schnell!");
   
   // LED ausschalten
   digitalWrite(LED_PIN, LOW);
@@ -46,7 +48,7 @@ void loop() {
     isClicking = !isClicking;
     
     if (isClicking) {
-      Serial.println("Auto-Clicking GESTARTET (1000 Hz)");
+      Serial.println("Auto-Clicking GESTARTET (10 kHz = 10.000 Klicks/Sek)");
       digitalWrite(LED_PIN, HIGH); // LED an
     } else {
       Serial.println("Auto-Clicking GESTOPPT");
@@ -58,19 +60,29 @@ void loop() {
   
   // Auto-Clicking ausführen wenn aktiviert
   if (isClicking) {
-    unsigned long currentTime = millis();
+    unsigned long currentTimeMicros = micros();
     
-    // Prüfen ob 1ms vergangen ist (1000 Hz)
-    if (currentTime - lastClickTime >= clickInterval) {
+    // Prüfen ob 100 Mikrosekunden vergangen sind (10 kHz)
+    if (currentTimeMicros - lastClickTime >= clickIntervalMicros) {
       // Linke Maustaste klicken (press und release)
       Mouse.click(MOUSE_LEFT);
       
-      lastClickTime = currentTime;
+      lastClickTime = currentTimeMicros;
+      clickCounter++;
+      
+      // Status-Update alle 10000 Klicks (jede Sekunde bei 10 kHz)
+      if (clickCounter >= 10000) {
+        Serial.print("10.000 Klicks gesendet (1 Sekunde) - Total: ");
+        Serial.println(clickCounter);
+        clickCounter = 0;
+      }
     }
   }
   
-  // Kleine Verzögerung um CPU-Last zu reduzieren
-  delayMicroseconds(100);
+  // Minimale CPU-Entlastung
+  if (!isClicking) {
+    delayMicroseconds(100);
+  }
 }
 
 // Funktion um Auto-Clicking über Serial-Kommandos zu steuern
@@ -82,7 +94,7 @@ void serialEvent() {
     if (command == "start") {
       isClicking = true;
       digitalWrite(LED_PIN, HIGH);
-      Serial.println("Auto-Clicking via Serial GESTARTET");
+      Serial.println("Auto-Clicking via Serial GESTARTET (10 kHz)");
     }
     else if (command == "stop") {
       isClicking = false;
